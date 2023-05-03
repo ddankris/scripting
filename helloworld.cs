780e3162aa435be2e942e6f27a75614cc723b1cf
ddankris <131892424+ddankris@users.noreply.github.com>
2023-05-03 17:23:48 +0300

    привет

---
+++ helloworld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль привет
+        print("Hello");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
